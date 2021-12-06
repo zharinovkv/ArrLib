37f968ede84a950cfc3fad05179f27dcfcbb99d8
Konstantin Zharinov <zharinovkv@gmail.com>
2021-12-06 22:47:24 +0500

    Rename file

---
+++ ArrLib/Numbers.cs
@@ -0,0 +1,23 @@
+﻿namespace ArrayLibrary;
+
+public class Numbers
+{
+    public int[] Nums;
+
+    public Numbers(ArrayPreparer preparer)
+    {
+        try
+        {
+            Nums = preparer.Nums;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Возникло исключение {ex.Message}");
+        }
+    }
+
+    public int sumOfTwoMinNumbers()
+    {
+        return Nums.OrderBy(n => n).Take(2).Sum();
+    }
+}
