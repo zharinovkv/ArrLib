26503014e790213e69c0fcdd354148fa4f14dcbf
Konstantin Zharinov <zharinovkv@gmail.com>
2021-12-18 21:15:28 +0500

    Delite DataContainer

--- ArrLib/DataContainer.cs
+++
@@ -1,9 +0,0 @@
-namespace ArrayLibrary;
-
-public class DataContainer
-{
-    public int intValue { get; set; } = 0;
-    public double doubleValue { get; set; } = 0.0;
-    public string stringValue { get; set; } = "";
-    public int[] arrayValue { get; set; } = new int[] { };
-}
