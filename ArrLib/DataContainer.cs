6409745b06413d3abe7390f61bd28b9099853ed3
Konstantin Zharinov <zharinovkv@gmail.com>
2021-12-18 21:11:14 +0500

    Add DataContainer

---
+++ ArrLib/DataContainer.cs
@@ -0,0 +1,9 @@
+namespace ArrayLibrary;
+
+public class DataContainer
+{
+    public int intValue { get; set; } = 0;
+    public double doubleValue { get; set; } = 0.0;
+    public string stringValue { get; set; } = "";
+    public int[] arrayValue { get; set; } = new int[] { };
+}
