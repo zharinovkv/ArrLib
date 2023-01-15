603fe9688499586a384ab5b1d31948c996b23a3e
Konstantin Zharinov <zharinovkv@gmail.com>
2023-01-15 22:53:36 +0500

    refactoring

--- ArrLib/Numbers.cs
+++ ArrLib/Numbers.cs
@@ -2,21 +2,21 @@
 
 public class Numbers
 {
-    private string Result;
+    private readonly string? Result = string.Empty;
 
     private Dictionary<string, Func<int[], int, string>> Funcs =
         new Dictionary<string, Func<int[], int, string>>
     {
         {"sum2MinNums", (nums, slice) => isValidArray(nums, 2) ?
             nums.OrderBy(n => n).Take(2).Sum().ToString() : ""},
-        {"sumSliceNums", (nums, slice) => isValidArray(nums, slice) ?
+        {"sumSliceNums", (nums, slice) => isValidArray(nums, 1) ?
             nums.Take(slice).Sum().ToString() : ""},
         {"averageNums", (nums, slice) => isValidArray(nums, 1) ?
-            nums.Average().ToString() : ""},
+            nums.Take(slice).Average().ToString() : ""},
         {"toString", (nums, slice) => isValidArray(nums, 1) ?
-            string.Join(" ", nums).ToString() : ""},
+            String.Join(" ", nums.Take(slice)).ToString() : ""},
         {"oddNums", (nums, slice) => isValidArray(nums, 1) ?
-            String.Join(", ", nums.Where(n => n % 2 != 0).ToArray()) : ""}
+            String.Join(" ", nums.Take(slice).Where(n => n % 2 != 0).ToArray()) : ""}
     };
 
     public Numbers(string func, int[] nums, int slice = 0)
--- ShowCase/Program.cs
+++ ShowCase/Program.cs
@@ -4,10 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] nums = { 4, 0, 3, 19, 492, -10, 1 };
+        int[] nums = { 4, 0, 3, 19, 492, -10, 1 };        
 
-        var result = new Numbers("sum2MinNums", nums).getValue();
-
-        Console.WriteLine(result);
+        Console.WriteLine(new Numbers("sum2MinNums", nums).getValue());
+        Console.WriteLine(new Numbers("sumSliceNums", nums, 6).getValue());
+        Console.WriteLine(new Numbers("averageNums", nums, 5).getValue());
+        Console.WriteLine(new Numbers("toString", nums, 4).getValue());
+        Console.WriteLine(new Numbers("oddNums", nums, 3).getValue());
     }
 }
+
